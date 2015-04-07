7b8db7971b2828e723b8303c9a8885e0f309f7dc
Seth Howard <smiles.seth@gmail.com>
2015-04-07 17:54:02 -0600

    initial commit

---
+++ main.js
@@ -0,0 +1,8 @@
+console.log("Application is running :)");
+
+document.querySelector('button#go').addEventListener('click', go);
+
+function go() {
+    // Write code here
+    console.log('you clicked go!');
+}
